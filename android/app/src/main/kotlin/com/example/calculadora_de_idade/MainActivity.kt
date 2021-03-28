44af115d908189a1eb833b0cdb2d14b57340edf1
Deivisson Altoé <deivealtoe@gmail.com>
2021-03-28 16:16:05 -0300

    First

---
+++ android/app/src/main/kotlin/com/example/calculadora_de_idade/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.calculadora_de_idade
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,148 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+
+void main() => runApp(
+      MaterialApp(
+        localizationsDelegates: [
+          GlobalWidgetsLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+        ],
+        title: 'Calculadora de Idade',
+        home: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.orange,
+            title: Text(
+              'Calculadora de Idade',
+            ),
+          ),
+          body: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: FormAndLabels(),
+            ),
+          ),
+        ),
+      ),
+    );
+
+class FormAndLabels extends StatefulWidget {
+  @override
+  _FormAndLabelsState createState() {
+    return _FormAndLabelsState();
+  }
+}
+
+class _FormAndLabelsState extends State<FormAndLabels> {
+  DateTime _birthdayDate;
+
+  String _birthdayDateButtonText = 'Sua Data de Nascimento';
+
+  int _timeAliveInDays = 0;
+  int _timeAliveInHours = 0;
+  int _timeAliveInMinutes = 0;
+  int _timeAliveInSeconds = 0;
+  int _timeAliveInMilliseconds = 0;
+  int _timeAliveInMicroseconds = 0;
+
+  Future<void> _getBirthdayDate() async {
+    _birthdayDate = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: new DateTime(1900),
+        lastDate: new DateTime.now(),
+        locale: Locale('pt', 'BR'));
+
+    DateTime rightNow = new DateTime.now();
+
+    _timeAliveInDays = rightNow.difference(_birthdayDate).inDays;
+    _timeAliveInHours = rightNow.difference(_birthdayDate).inHours;
+    _timeAliveInMinutes = rightNow.difference(_birthdayDate).inMinutes;
+    _timeAliveInSeconds = rightNow.difference(_birthdayDate).inSeconds;
+    _timeAliveInMilliseconds =
+        rightNow.difference(_birthdayDate).inMilliseconds;
+    _timeAliveInMicroseconds =
+        rightNow.difference(_birthdayDate).inMicroseconds;
+
+    _birthdayDateButtonText =
+        '${_birthdayDate.day}/${_birthdayDate.month}/${_birthdayDate.year}';
+
+    print('Birth date chosen: $_birthdayDateButtonText');
+
+    setState(() {
+      return _birthdayDate;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        TextButton(
+          style: TextButton.styleFrom(
+            padding: EdgeInsets.only(bottom: 20),
+          ),
+          onPressed: _getBirthdayDate,
+          child: Container(
+            padding: EdgeInsets.only(top: 13, bottom: 13),
+            color: Colors.orange,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 20),
+                  child: Text(
+                    _birthdayDateButtonText,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                Icon(
+                  Icons.calendar_today_outlined,
+                  color: Colors.white,
+                  size: 30,
+                ),
+              ],
+            ),
+          ),
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: Colors.orange,
+            borderRadius: BorderRadius.all(
+              Radius.circular(
+                35,
+              ),
+            ),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              getTextLabeled('Dias: $_timeAliveInDays'),
+              getTextLabeled('Horas: $_timeAliveInHours'),
+              getTextLabeled('Minutos: $_timeAliveInMinutes'),
+              getTextLabeled('Segundos: $_timeAliveInSeconds'),
+              getTextLabeled('Milisegundos: $_timeAliveInMilliseconds'),
+              getTextLabeled('Microsegundos: $_timeAliveInMicroseconds'),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+Widget getTextLabeled(String labelString) {
+  return Container(
+    padding: EdgeInsets.only(left: 30, top: 25, bottom: 25),
+    child: Text(
+      '$labelString',
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 18,
+      ),
+    ),
+  );
+}
